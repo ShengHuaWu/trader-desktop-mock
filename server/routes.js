5f05a6334b7a99095c06e9cad21f482a2da94b80
Shane Wu <shane.wu@nogle.com>
2016-04-15 16:47:06 +0800

    Finish trader desktop api.

--- server/routes.js
+++ server/routes.js
@@ -2,12 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
+const accountId = 'laphone';
+const engineId = '5566';
+const modelId = '55';
+const configId = '484';
+
 // Auth
 router.post('/auth/login', (req, res) => {
-  if (req.body.id === 'laphone' && req.body.pass === 'laphone') {
-    res.status(200).json({'accountId' : req.body.id});
+  if (req.body.id === accountId && req.body.pass === accountId) {
+    res.status(200).json({ 'accountId' : req.body.id });
   } else {
-    res.status(401).json({'errorcd' : 1, 'errormsg' : 'You are NOT Laphone.'});
+    res.status(401).json({ 'errorCode' : 1, 'errorMsg' : 'You are NOT Laphone.' });
   }
 });
 
@@ -22,7 +27,7 @@ router.get('/auth/islogin', (req, res) => {
 // Symbols
 router.get('/symbols/fuzzyquery', (req, res) => {
   if (req.query.symbol === 'cu') {
-    res.status(200).json(['cu1611', 'cu1606', 'cu1517']);
+    res.status(200).json([ 'cu1611', 'cu1606', 'cu1517' ]);
   } else {
     res.status(200).json([]);
   }
@@ -33,7 +38,7 @@ router.get('/engines/cpuusage', (req, res) => {
   const engines = [
     {
       'eid' : '5566',
-      'cpuusage' : 99,
+      'cpuusage' : 97,
       'running' : true
     },
     {
@@ -48,9 +53,9 @@ router.get('/engines/cpuusage', (req, res) => {
 // Accounts
 router.get('/accounts/info', (req, res) => {
   const info = {
-      'traderAccount' : 'laphone',
-      'traderPass' : 'laphone',
-      'traderName' : 'laphone',
+      'traderAccount' : accountId,
+      'traderPass' : accountId,
+      'traderName' : accountId,
       'balance' : 150000.0,
       'pnl' : 0.0,
       'margin' : 20000.0,
@@ -67,15 +72,11 @@ router.get('/accounts/info', (req, res) => {
 });
 
 router.get('/accounts/marginusage', (req, res) => {
-  const info = {
-      'accountId' : 'laphone',
-      'marginUsage' : 0
-  };
-  res.status(200).json(info);
+  res.status(200).json({ 'accountId' : accountId, 'marginUsage' : 0 });
 });
 
 router.get('/accounts/isacceptable', (req, res) => {
-  if (req.query.accountId === 'laphone') {
+  if (req.query.accountId === accountId) {
     res.status(406).end();
   } else {
     res.status(200).end();
@@ -84,7 +85,7 @@ router.get('/accounts/isacceptable', (req, res) => {
 
 router.get('/accounts/pnl', (req, res) => {
   const pnl = {
-    'accountId' : 'laphone',
+    'accountId' : accountId,
     'floatingPnl' : 0,
     'closePnl' : 0,
     'commission' : 0
@@ -95,7 +96,7 @@ router.get('/accounts/pnl', (req, res) => {
 // Models
 router.get('/models', (req, res) => {
   const models = {
-    'trade-model-1.0.Laphone.jar': [
+    'trade-model-1.0.jar': [
       'LaphoneModel2',
       'LaphoneM1',
     ],
@@ -107,15 +108,15 @@ router.get('/models', (req, res) => {
 });
 
 router.get('/models/symbols', (req, res) => {
-  if (req.query.eid === '5566' && req.query.modelId === '47') {
-    res.status(200).json(['cu1611', 'cu1606', 'cu1517']);
+  if (req.query.eid === engineId && req.query.modelId === modelId) {
+    res.status(200).json([ 'cu1611', 'cu1606', 'cu1517' ]);
   } else {
     res.status(200).json([]);
   }
 });
 
 router.get('/models/pnl', (req, res) => {
-  if (req.query.eid === '5566' && req.query.modelId === '47') {
+  if (req.query.eid === engineId && req.query.modelId === modelId) {
     const pnl = {
       'modelId' : '47',
       'floatingPnl' : 0,
@@ -129,7 +130,7 @@ router.get('/models/pnl', (req, res) => {
 });
 
 router.get('/models/blotter', (req, res) => {
-  if (req.query.eid === '5566' && req.query.modelId === '47') {
+  if (req.query.eid === engineId && req.query.modelId === modelId) {
     const blotters = [
       {
         'sqn' : 'abc',
@@ -190,7 +191,7 @@ router.get('/models/configs', (req, res) => {
     ],
     'classes' : [
       {
-        'fileName' : 'trade-model-1.0.Laphone.jar',
+        'fileName' : 'trade-model-1.0.jar',
         'classes' : [
           'com.nogle.models.breakthrough.BreakthroughStrategyBuilder',
           'com.nogle.models.ZincModelBuilder',
@@ -209,6 +210,113 @@ router.get('/models/config/isacceptable', (req, res) => {
   } else {
     res.status(200).end();
   }
+});
+
+router.get('/models/config', (req, res) => {
+  if (req.query.id === configId) {
+    const config = {
+      'id' : configId,
+      'strategyName' : 'RegressionTestingModel',
+      'fileName' : 'trade-model-1.0.jar',
+      'strategyFullPath' : 'com.nogle.models.RegressionTestingModelBuilder',
+      'closingOffset' : 55.0,
+      'maxPositionPerSide' : 66.0,
+      'maxOrderQuantity' : 55,
+      'maxOrdersPerSide' : 66,
+      'minOrderQuantity' : 55,
+      'maxCrossTicks' : 66.0,
+      'subsequentOrderDelay' : 55.0,
+      'minCancelDelay' : 66.0,
+      'Schedule' : ["11:00:00-12:00:00"],
+      'Contrast' : [
+        {
+          'name' : 'abc',
+          'value' : 'cu@SHFE'
+        }
+      ]
+    };
+    res.status(200).json(config);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.post('/models/config', (req, res) => {
+  if (typeof req.body.strategyName !== 'undefined') {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 602, 'errorMsg' : 'Insert model config failed.' });
+  }
+});
+
+router.put('/models/config', (req, res) => {
+  if (typeof req.body.id !== 'undefined') {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 603, 'errorMsg' : 'Update model config failed.' });
+  }
+});
+
+router.delete('/models/config', (req, res) => {
+  if (typeof req.query.id !== 'undefined') {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 604, 'errorMsg' : 'Delete model config failed.' });
+  }
+});
+
+// Commands
+router.get('/commands', (req, res) => {
+  if (req.query.eid === engineId && req.query.modelId === modelId) {
+    const commands = [
+      {
+        'command' : 'fill',
+        'usage' : 'abc',
+        'desc' : 'xyz'
+      }
+    ];
+    res.status(200).json(commands);
+  } else {
+    res.status(400).json({ 'errorCode' : 200, 'errorMsg' : 'Query command failed.' });
+  }
+});
+
+router.post('/commands', (req, res) => {
+  if (typeof req.body.eid === engineId && req.body.modelId === modelId) {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 201, 'errorMsg' : 'Send command failed.' });
+  }
+});
+
+router.post('/commands/models/enable', (req, res) => {
+  if (typeof req.body.eid === engineId && req.body.modelId === modelId) {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 101, 'errorMsg' : 'Enable model failed.' });
+  }
+});
+
+router.post('/commands/models/disable', (req, res) => {
+  if (typeof req.body.eid === engineId && req.body.modelId === modelId) {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 102, 'errorMsg' : 'Disable model failed.' });
+  }
+});
+
+// Order
+router.post('/order', (req, res) => {
+  if (typeof req.body.eid === engineId && req.body.modelId === modelId) {
+    res.status(200).end();
+  } else {
+    res.status(400).json({ 'errorCode' : 800, 'errorMsg' : 'Send order failed.' });
+  }
+});
+
+// Other
+router.get('/brokers', (req, res) => {
+  res.status(200).json([ 'HT', 'JR', 'QQ', 'TEST' ]);
 });
 
 export {router};
